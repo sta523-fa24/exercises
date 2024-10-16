6da07de06fe995ba11137477bbd15cdeecc23626
Colin Rundel <rundel@gmail.com>
2024-10-16 10:02:29 -0400

    Runner

---
+++ class_2024-10-16_plumber-examples/run.R
@@ -0,0 +1,12 @@
+job::empty({
+  plumber::plumb(file='class_2024-10-16_plumber-examples/ex1.R')$run()
+})
+
+job::empty({
+  plumber::plumb(file='class_2024-10-16_plumber-examples/ex2.R')$run()
+})
+
+
+job::empty({
+  plumber::plumb(file='class_2024-10-16_plumber-examples/ex3.R')$run()
+})
