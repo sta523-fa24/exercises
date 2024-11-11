f0d4b68949a16ac86acc6d6e1f1a9cb9e1f1b0f1
Colin Rundel <rundel@gmail.com>
2024-11-11 08:30:12 -0500

    prep

---
+++ class-2024-11-11.R
@@ -0,0 +1,66 @@
+library(tidyverse)
+
+# Data setup
+
+set.seed(11112024)
+d = data.frame(x = 1:120) |>
+  mutate(y = sin(2*pi*x/120) + runif(length(x),-1,1))
+
+l = loess(y ~ x, data=d)
+p = predict(l, se=TRUE)
+
+d = d |> mutate(
+  pred_y = p$fit,
+  pred_y_se = p$se.fit
+)
+
+# Initial visualization
+
+g = ggplot(d, aes(x,y)) +
+  geom_point(color="gray50") +
+  geom_ribbon(
+    aes(ymin = pred_y - 1.96 * pred_y_se, 
+        ymax = pred_y + 1.96 * pred_y_se), 
+    fill="red", alpha=0.25
+  ) +
+  geom_line(aes(y=pred_y)) +
+  theme_bw()
+
+g
+
+# Bootstrap
+
+n_rep = 500
+
+bs = purrr::map_dfr(
+  seq_len(n_rep),
+  function(i) {
+    d |>
+      select(x, y) |>
+      slice_sample(prop = 1, replace = TRUE) |>
+      ( \(df) {
+        mutate(
+          df, iter = i,
+          pred = loess(y ~ x, data = df) |> predict()
+        )
+      })()
+  }
+) |>
+  group_by(x, y) |>
+  summarize(
+    bs_low = quantile(pred, probs = 0.025),
+    bs_upp = quantile(pred, probs = 0.975),
+    .groups = "drop"
+  )
+
+g +
+  geom_ribbon(
+    data = bs,
+    aes(ymin = bs_low, ymax = bs_upp),
+    color = "blue", alpha = 0.25
+  )
+
+
+# Multisession Bootstrap
+
+future::plan(future::multisession)
